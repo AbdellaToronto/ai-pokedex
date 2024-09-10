12d4e479dd710d8d898982b44a4a6be6b4703783
Della <abdellaalioncan@gmail.com>
2024-09-10 10:07:31 -0400

    up max duration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { PokemonComponent } from "@/components/pokemon-component";
 
+export const maxDuration = 60;
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between h-screen">
